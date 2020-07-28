6baec8363662f5445365982c1ef4474fa86ffa17
Mika Green <Damikag@gmail.com>
2020-07-27 20:18:57 -0400

    Added TestRunner

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,26 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //Path to your feature file
+        features = {"src/main/resources/Feature"},
+
+        // Specify step definition package name (Note: make sure to have this package on current directory)
+        glue = {"StepDefinitions", "Drivers"},
+
+        // This creates cucumber reports
+        plugin = {"pretty", "html:Reports/Default/"},
+
+        // This create auto method name in camelCase
+        snippets = cucumber.api.SnippetType.CAMELCASE,
+
+        // Specify tags to be executed
+        tags = {"@sky"},
+
+        // readable console logs
+        monochrome = true
+)
+public class TestRunner {
+}
--- target/classes/Feature/DarkSky/TempSummary.feature
+++ target/classes/Feature/DarkSky/TempSummary.feature
@@ -1,5 +1,6 @@
 Feature: Temp Summary
 
+  @sky
   Scenario: Verify the temp value changes as per temp unit selected by user
     Given I am on darksky landing page
     Then I verify feelsLike, low and high temp values are displayed
